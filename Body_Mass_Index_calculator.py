23bd7a49dec59135cd6ad9fb97b795b593fc427d
Shaker Alghaithi <ghaithishake@gmail.com>
2021-12-25 14:21:48 -0500

    Added the body mass calculator solution

---
+++ Body_Mass_Index_calculator.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+int_height = float(height)**2
+int_weight = int(weight)
+BMI = int_weight / int_height
+print (round(BMI))
+
+
+
