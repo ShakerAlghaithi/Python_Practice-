cce116cba032a74a8c82d2fa9ea70debc8b80110
Shaker Alghaithi <ghaithishake@gmail.com>
2022-06-01 16:09:05 -0400

    adding sum of the even numbers  exercise

---
+++ even_numbers_sum.py
@@ -0,0 +1,15 @@
+# Instructions
+
+# You are going to write a program that calculates the sum of all the even numbers from 1 to 100. Thus, the first even number would be 2 and the last one is 100:
+
+# i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+
+# Important, there should only be 1 print statement in your console output. It should just print the final total and not every step of the calculation.
+
+
+#Write your code below this row 👇
+even_num =0
+for n in range(2, 101):
+    if n%2 ==0:
+        even_num+= n 
+print(f"The sum of the even numbers in range 2 - 100 is : {even_num}")
